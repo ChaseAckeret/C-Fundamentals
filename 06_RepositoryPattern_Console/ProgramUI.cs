a2488a51983c1f0bcc1246677b7ab7dec78caf26
ChaseAckeret <chaseackeret@gmail.com>
2020-11-21 17:15:36 -0500

    2nd push

--- 06_RepositoryPattern_Console/ProgramUI.cs
+++ 06_RepositoryPattern_Console/ProgramUI.cs
@@ -9,9 +9,13 @@ namespace _06_RepositoryPattern_Console
 {
     class ProgramUI
     {
+        private StreamingContent_Repository _contentRepo = new StreamingContent_Repository();
+
+
         // Method that runs/starts the application
         public void Run()
         {
+            SeedContentList();
             Menu();
         }
 
@@ -75,31 +79,204 @@ namespace _06_RepositoryPattern_Console
         //Create New Streaming Content
         private void CreateNewContent()
         {
-            StreamingContent
+            Console.Clear();
+            StreamingContent newContent = new StreamingContent();
+
+            //Title
+            Console.WriteLine("Enter the title for the content:");
+            newContent.Title = Console.ReadLine();
+
+            //Description
+            Console.WriteLine("Enter the description for the content:");
+            newContent.Description = Console.ReadLine();
+
+            //Maturity Rating
+            Console.WriteLine("Enter the rating for the content (G, PG, PG-13, etc");
+            newContent.MaturityRating = Console.ReadLine();
+
+            //Star Rating
+            Console.WriteLine("Enter the star count for the content (5.8, 10, 1.5, etc");
+            string starsAsString = Console.ReadLine();
+            newContent.StarRating = double.Parse(starsAsString);
+
+            //Is Family Friendly
+            Console.WriteLine("Is this content family friendly? (y/n)");
+            string familyFriendlyString = Console.ReadLine().ToLower();
+            if (familyFriendlyString=="y")
+            {
+                newContent.IsFamilyFriendly = true;
+            }
+            else
+            {
+                newContent.IsFamilyFriendly = false;
+            }
+
+            //Genre Type
+            Console.WriteLine("Enter the Genre Number \n" +
+                "1: Horror \n" +
+                "2: Romcom \n" +
+                "3: Scifi \n" +
+                "4: Documentary \n" +
+                "5: Bromancce \n" +
+                "6: Drama \n" +
+                "7: Action \n");
+            string genreAsString = Console.ReadLine();
+            int genreAsInt = int.Parse(genreAsString);
+            newContent.TypeOfGenre = (GenreType)genreAsInt;
+
+            _contentRepo.AddContentToList(newContent);
+
         }
 
         //View Current Streaming Content that  is saved
         private void DisplayAllContent()
         {
+            Console.Clear();
+            List<StreamingContent> listOfContent = _contentRepo.GetContentList();
 
+            foreach(StreamingContent content in listOfContent)
+            {
+                Console.WriteLine($"Title: {content.Title}, \n" +
+                    $" Description: {content.Description}");
+            }
         }
 
         //View existing content by title
         private void DisplayContentByTitle()
         {
+            Console.Clear();
 
+            //Prompt the user to give a title
+            Console.WriteLine("Enter the title of the content you'd like to see:");
+
+            //Get the user's input
+            string title = Console.ReadLine();
+
+            //Find the content by that title
+            StreamingContent content = _contentRepo.GetContentByTitle(title);
+
+            //Display said content if it isn't null
+            if(content != null)
+            {
+                Console.WriteLine($"Title: {content.Title}\n" +
+                   $"Description: {content.Description}\n" +
+                   $"MaturityRating: {content.MaturityRating}\n" +
+                   $"Stars: {content.StarRating}\n" +
+                   $"Is Family Friendly: {content.IsFamilyFriendly}\n" +
+                   $"Genre: {content.TypeOfGenre}");
+            }
+            else
+            {
+                Console.WriteLine("No content by that title.");
+            } 
         }
 
         //Update Existing Content
         private void UpdateExistingContent()
         {
+            //Display all content
+            DisplayAllContent();
 
+            //Ask for the title to update
+            Console.WriteLine("Enter the title of the content you'd like to update.");
+
+            //Get that title
+            string oldTitle = Console.ReadLine();
+
+            //We will build a new object
+            Console.Clear();
+            StreamingContent newContent = new StreamingContent();
+
+            //Title
+            Console.WriteLine("Enter the title for the content:");
+            newContent.Title = Console.ReadLine();
+
+            //Description
+            Console.WriteLine("Enter the description for the content:");
+            newContent.Description = Console.ReadLine();
+
+            //Maturity Rating
+            Console.WriteLine("Enter the rating for the content (G, PG, PG-13, etc");
+            newContent.MaturityRating = Console.ReadLine();
+
+            //Star Rating
+            Console.WriteLine("Enter the star count for the content (5.8, 10, 1.5, etc");
+            string starsAsString = Console.ReadLine();
+            newContent.StarRating = double.Parse(starsAsString);
+
+            //Is Family Friendly
+            Console.WriteLine("Is this content family friendly? (y/n)");
+            string familyFriendlyString = Console.ReadLine().ToLower();
+            if (familyFriendlyString == "y")
+            {
+                newContent.IsFamilyFriendly = true;
+            }
+            else
+            {
+                newContent.IsFamilyFriendly = false;
+            }
+
+            //Genre Type
+            Console.WriteLine("Enter the Genre Number \n" +
+                "1: Horror \n" +
+                "2: Romcom \n" +
+                "3: Scifi \n" +
+                "4: Documentary \n" +
+                "5: Bromancce \n" +
+                "6: Drama \n" +
+                "7: Action \n");
+            string genreAsString = Console.ReadLine();
+            int genreAsInt = int.Parse(genreAsString);
+            newContent.TypeOfGenre = (GenreType)genreAsInt;
+
+            //Verify the Update Work
+            bool wasUpdated = _contentRepo.UpdateExisitingContent(oldTitle, newContent);
+
+            if (wasUpdated)
+            {
+                Console.WriteLine("Content successfully updated");
+            }
+            else
+            {
+                Console.WriteLine("Could not update content.");
+            } 
         }
 
         //Delete Existing Content
         private void DeleteExistingContent()
         {
+            DisplayAllContent();
 
+            //Prompt the user
+            Console.WriteLine("\nEnter the title of the content you'd like to remove.");
+            string input = Console.ReadLine();
+
+            //Call the delete method
+            bool wasDeleted = _contentRepo.RemoveContentFromList(input);
+
+            //If the content was deleted say so
+            if (wasDeleted)
+            {
+                Console.WriteLine("The content was successfully deleted.");
+            }
+            else
+            {
+                Console.WriteLine("The content could not be deleted.");
+            }
+
+            //Otherwise state it could not be deleted
+        }
+
+        //Seed method
+        private void SeedContentList()
+        {
+            StreamingContent sharknado = new StreamingContent("Sharknado", "Tornados, but with sharks", "TV-14", 3.3, true, GenreType.Action);
+            StreamingContent theRoom = new StreamingContent("The Room", "Banker's life gets turned upside down.", "R", 3.7, false, GenreType.Drama);
+            StreamingContent rubber = new StreamingContent("Rubber", "Car tire comes to life and goes on a killing spree.", "R", 5.8, false, GenreType.Documentary);
+
+            _contentRepo.AddContentToList(sharknado);
+            _contentRepo.AddContentToList(theRoom);
+            _contentRepo.AddContentToList(rubber);
         }
     }
 }
--- 06_RepositoryPattern_Tests/StreamingContentTests.cs
+++ 06_RepositoryPattern_Tests/StreamingContentTests.cs
@@ -4,11 +4,12 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace _06_RepositoryPattern_Tests
 {
     [TestClass]
-    public class UnitTest1
+    public class StreamingContentTests
     {
         [TestMethod]
         public void TestMethod1()
         {
+
         }
     }
 }
